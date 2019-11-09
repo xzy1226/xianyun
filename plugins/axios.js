3d7a442cf95ceb7a4b0caed658676db95ee4e449
源仙笙 <xzy_1226@163.com>
2019-11-09 21:01:14 +0800

    使用axios的拦截器拦截页面的所有请求错误

---
+++ plugins/axios.js
@@ -0,0 +1,9 @@
+import {Message} from "element-ui";
+
+// 使用axios的拦截器拦截页面的所有请求错误
+export default ({$axios, redirect})=>{
+  $axios.onError(err=>{
+    const {statusCode,message}=err.response.data;
+    statusCode===400 && Message.warning({message})
+  })
+}
--- store/actions.js
+++ store/actions.js
@@ -1,35 +1,18 @@
 export default {
   //登录
   async login({commit},data){
-    try {
-      // 发送请求
-      const res=await this.$axios.post("/accounts/login", data);
-      // 提交数据
-      commit('setUserInfo',res.data);
-      return res.data;
-    } catch (e) {
-      const {statusCode,message}=e.response.data
-      if (statusCode === 400) {
-        //提示信息
-        this._vm.$message({
-          message: message,
-          type: "error"
-        })
-        throw e.response;
-      }
-    }
+    // 发送请求
+    const res=await this.$axios.post("/accounts/login", data);
+    // 提交数据
+    commit('setUserInfo',res.data);
+    return res.data;
   },
   //注册
   async register({commit},data){
-    try {
-      //发送请求
-      const res=await this.$axios.post("/accounts/register",data);
-      //提交数据
-      commit('setUserInfo',res.data);
-      return res.data;
-    } catch (e) {
-      throw e.response;
-    }
+    //发送请求
+    const res=await this.$axios.post("/accounts/register",data);
+    commit('setUserInfo',res.data);
+    return res.data;
   },
 
   //验证手机号码
