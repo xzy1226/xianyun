0847e8396c5136fed75447993e2e471b102b2cce
源仙笙 <xzy_1226@163.com>
2019-11-12 20:32:25 +0800

    实现分页功能

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -15,8 +15,17 @@
                 
                 <!-- 航班信息 -->
                 <div>
-                    <FlightsItem v-for="(item,index) in flightsData.flights" :key="index" :data="item" />
+                    <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
                 </div>
+                <el-pagination
+                  background
+                  @size-change="handleSizeChange"
+                  @current-change="handleCurrentChange"
+                  :current-page="pageIndex"
+                  :page-sizes="[5, 10, 15, 20]"
+                  layout="total, sizes, prev, pager, next, jumper"
+                  :total="flightsData.total">
+                </el-pagination>
             </div>
 
             <!-- 侧边栏 -->
@@ -36,15 +45,38 @@ export default {
   components: {FlightsListHead,FlightsItem},
     data(){
         return { 
-            flightsData:{}
+            flightsData:{},   // 航班总数据
+            dataList: [],     // 当前页航班数据列表
+            pageIndex: 1,     // 当前页码
+            pageSize: 5       // 当前页数
         }
     },
     async mounted() {
+      // 设置传参数据
       const props={
         params: this.$route.query
       }
       this.flightsData=(await this.$store.dispatch('getAirs',props)).data
-      console.log(this.flightsData);
+      
+      this.setDataList()
+    },
+    methods: {
+      // 设置当前页的数量
+      setDataList(){
+        const start=(this.pageIndex-1)*this.pageSize;
+        const end=start+this.pageSize;
+        this.dataList=this.flightsData.flights.slice(start,end);
+      },
+      // 改变每页页数
+      handleSizeChange(val) {
+        this.pageSize=val;
+        this.setDataList()
+      },
+      // 改变当前页
+      handleCurrentChange(val) {
+        this.pageIndex=val;
+        this.setDataList()
+      }
     },
 }
 </script>
